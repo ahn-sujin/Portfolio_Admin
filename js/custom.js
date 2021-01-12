ef7f330d531aef53c003bc71d26520f052acdee7
sujin <asj9674@gmail.comgit config --global user.email asj9674@gmail.com>
2021-01-12 13:49:32 +0900

    upload admin page
    form ui and login page

---
+++ js/custom.js
@@ -0,0 +1,34 @@
+$(function(){
+
+    $('a[href="#"]').click(function(e) {
+        e.preventDefault();
+    });
+
+//gnb
+    $('#gnb>li>a').off('click');
+    $('#gnb>li>a').on('click',function(){
+        $('.submenu').stop().slideUp();
+        $(this).next('.submenu').stop().slideToggle();
+    });
+
+    $('.toggle').on('click',function(){
+        $('#header , #nav, #main').toggleClass('on');
+    });
+
+//datepicker
+    $('#date1').datepicker();
+    $('#date2').datepicker();
+    setDatePickerBtn('.date1');
+    setDatePickerBtn('.date2');
+    function setDatePickerBtn(selector){
+        $(selector).on('click',function(){
+            $(this).prev().focus();
+        });
+    }
+    
+
+    
+
+
+
+});
